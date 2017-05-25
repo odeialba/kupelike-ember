632267155a6d97e69bf7855ff5246d0e50955814
oalba <odeialba@yahoo.es>
2017-05-25 16:23:35 +0200

    everything works2

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -3,17 +3,17 @@ import Config from '../config/environment';
 
 export default Ember.Controller.extend({
 	actions: {
-        postEmail: function(){
-        	var that = this;
-            Ember.$.ajax({
+		postEmail: function(){
+			var that = this;
+			Ember.$.ajax({
 				url: Config.apiURL+"/contact",
 				type: "POST",
 				data: Ember.$('.contact_form').serialize(),
 				processData: false,
 				complete: function() {
 					that.replaceRoute('index2');
-		        }
+				}
 			});
-        }
-    }
+		}
+	}
 });
